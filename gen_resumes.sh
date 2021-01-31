caad9a01ad6294c1357f155a2f5bf5a62f050e7c
witekosz <witekosz@gmail.com>
2021-01-31 20:15:58 +0100

    Fix gen script

--- gen_resumes.sh
+++ gen_resumes.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# TODO Move generating to local package script
+
 THEMES=(macchiato onepage spartan stackoverflow)
-HTML_DIR=themes
+HTML_DIR=html
 PDF_DIR=pdf
 
 # Generate resumes
@@ -11,12 +13,18 @@ for theme in ${THEMES[*]}
 do
    echo "$theme"
    resume export "$HTML_DIR/resume-$theme.html" --theme $theme
+   resume export "$PDF_DIR/resume-$theme.pdf" --theme $theme
    echo ""
 done
 
-# Generate macchiato pdf
+# Generate and replace macchiato pdf
 # https://www.npmjs.com/package/jsonresume-theme-macchiato
 puppeteer --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 --format A4 \
     print "$HTML_DIR/resume-macchiato.html" "$PDF_DIR/resume-macchiato.pdf"
+
+echo "updating main resume"
+
+cp html/resume-macchiato.html resume.html
+cp pdf/resume-macchiato.pdf resume.pdf
 
 echo "finished generating resumes"
